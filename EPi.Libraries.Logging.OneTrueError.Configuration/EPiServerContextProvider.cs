ccd77dfa3905f341c96e5b84889301bc06268804
Jeroen Stemerdink <jeroen.development@outlook.com>
2017-02-16 15:09:29 +0100

    Replaced Jos.ContentJson dependency with IObjectSerializerFactory implementation from EPiServer

--- EPi.Libraries.Logging.OneTrueError.Configuration/EPiServerContextProvider.cs
+++ EPi.Libraries.Logging.OneTrueError.Configuration/EPiServerContextProvider.cs
@@ -1,13 +1,30 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="EPiServerContextProvider.cs" company="Valtech">
-//     Copyright © 2017 Valtech.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
+﻿// Copyright © 2016 Jeroen Stemerdink.
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 namespace EPi.Libraries.Logging.OneTrueError.Configuration
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
 
+    using EPiServer.Framework.Serialization;
     using EPiServer.Globalization;
     using EPiServer.ServiceLocation;
     using EPiServer.Web.Routing;
@@ -15,8 +32,6 @@ namespace EPi.Libraries.Logging.OneTrueError.Configuration
     using global::OneTrueError.Client.ContextProviders;
     using global::OneTrueError.Client.Contracts;
     using global::OneTrueError.Client.Reporters;
-
-    using Jos.ContentJson.Extensions;
 
     /// <summary>
     /// Class EPiServerContextProvider.
@@ -45,13 +60,31 @@ namespace EPi.Libraries.Logging.OneTrueError.Configuration
         public ContextCollectionDTO Collect(IErrorReporterContext context)
         {
             IContentRouteHelper contentRouteRouteHelper = null;
+            IObjectSerializerFactory serializerFactory = null;
+            string contentData = string.Empty;
 
             try
             {
                 contentRouteRouteHelper = ServiceLocator.Current.GetInstance<IContentRouteHelper>();
+                serializerFactory = ServiceLocator.Current.GetInstance<IObjectSerializerFactory>();
             }
             catch (ActivationException)
             {
+            }
+
+            if (contentRouteRouteHelper == null)
+            {
+                return new ContextCollectionDTO(this.Name, new Dictionary<string, string>());
+            }
+
+            if (serializerFactory != null)
+            {
+                // TODO: Check whether to use invariant culture 
+                using (StringWriter stringWriter = new StringWriter(ContentLanguage.PreferredCulture))
+                {
+                    serializerFactory.GetSerializer("application/json").Serialize(stringWriter, contentRouteRouteHelper.Content);
+                    contentData = stringWriter.ToString();
+                }
             }
 
             Dictionary<string, string> contextInfo = new Dictionary<string, string>
@@ -67,7 +100,7 @@ namespace EPi.Libraries.Logging.OneTrueError.Configuration
                                                              },
                                                              {
                                                                  "ContentData",
-                                                                 contentRouteRouteHelper?.Content?.ToJson()
+                                                                 contentData
                                                              }
                                                          };
 
